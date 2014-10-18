749f576b530d850cce0d4d39297219bb601d874c
Jon Shire <jos56@aber.ac.uk>
2014-10-18 17:02:42 +0100

    Changed the name to be correct.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, this is Jon Tetrino!");
+		System.out.println("Hello, this is Jon Shire!");
 	}
 
 }
