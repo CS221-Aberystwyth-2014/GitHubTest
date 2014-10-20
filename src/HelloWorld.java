81abcce1132a45c7acf48f2a8533d9e4312b704d
anh45 <anh45@aber.ac.uk>
2014-10-21 00:17:05 +0100

    Added my own msg

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, this is Jon Shire!");
+		System.out.println("Hello, this is Andreas Hernandez!");
 	}
-
 }
