a9b2ac8ae38f0c81d004ddd022286c4e8e6552f5
alm1 <maguire.alix@gmail.com>
2014-10-21 20:10:12 +0100

    Hello World
    Added my name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, this is Jon Shire!");
 		System.out.println("Hello, this is Andreas Hernandez!");
+        System.out.println("Hello, this is Alexandra Maguire!");
 	}
 }
