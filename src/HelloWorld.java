9f61b9cf1cbbb9beb66b77754696979a6ad26ccb
Rhydian Jenkins <RhydianJenkins@users.noreply.github.com>
2014-11-04 16:32:52 +0000

    added my name to the hello list

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, this is Jon Shire!");
 		System.out.println("Hello, this is Andreas Hernandez!");
-        System.out.println("Hello, this is Alexandra Maguire!");
+		System.out.println("Hello, this is Alexandra Maguire!");
+        	System.out.println("Hello, this is Rhyidna Jenkins!");
 	}
 }
